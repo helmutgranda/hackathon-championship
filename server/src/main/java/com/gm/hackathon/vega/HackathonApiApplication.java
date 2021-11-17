8324d8382b3244b85baf59f6ab0726f312cb04b1
Evan Edwards <evan.edwards.uni@gmail.com>
2021-11-17 12:47:16 -0600

    added spring project

---
+++ server/src/main/java/com/gm/hackathon/vega/HackathonApiApplication.java
@@ -0,0 +1,13 @@
+package com.gm.hackathon.vega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackathonApiApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/gm/hackathon/vega/HackathonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.hackathon.vega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
