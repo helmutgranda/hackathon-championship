2bc9a9830654c00136a2f0d5926fc473d63f8519
Garrett <gbrum2014@gmail.com>
2021-11-17 17:36:53 -0600

    First nav commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,14 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import {NavigationService} from "./navigation/navigation.service";
+import {MatListModule} from "@angular/material/list";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {RouterModule, Routes} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatButtonModule} from "@angular/material/button";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: AppComponent}
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatListModule,
+    MatSidenavModule,
+    RouterModule.forRoot(appRoutes),
+    MatCheckboxModule,
+    MatButtonModule
   ],
   providers: [NavigationService],
   bootstrap: [AppComponent]
