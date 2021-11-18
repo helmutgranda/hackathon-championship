5d95fd2f3a30cec3f51ab19c474688b854c9f1cb
hunter-sacky <hunter.sacky@gmail.com>
2021-11-18 02:54:23 -0600

    Created one of the learning experience modules

---
+++ client/src/app/learning-experience-one/learning-experience-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-experience-one',
+  templateUrl: './learning-experience-one.component.html',
+  styleUrls: ['./learning-experience-one.component.css']
+})
+export class LearningExperienceOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
