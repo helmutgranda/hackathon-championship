5584b5df04d9fab7c8dd878986a0f1f607f7d523
Helmut Granda <contact@helmutgranda.com>
2021-11-17 21:02:21 -0600

    Adding /profile to router

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 import { LoginComponent } from './login/login.component';
 import { SelectorComponent } from './selector/selector.component';
 import { EvAcademyComponent } from './ev-academy/ev-academy.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'ev-academy', component: EvAcademyComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'selector', component: SelectorComponent },
   { path: '**', component: NoPageFoundComponent }
